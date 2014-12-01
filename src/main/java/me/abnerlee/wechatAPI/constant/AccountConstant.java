625c91a1daebf5d7a47d38796e3310b73752379f
Abner Lee <657143946@qq.com>
2014-12-01 17:42:32 +0800

    store constant about your account of public platform of wechat, read from constantmap

---
+++ src/main/java/me/abnerlee/wechatAPI/constant/AccountConstant.java
@@ -0,0 +1,10 @@
+package me.abnerlee.wechatAPI.constant;
+
+/**
+ * Created by AbnerLee on 14-12-1.
+ */
+
+public class AccountConstant {
+    public final static String TOKEN = ConstantMap.ConstantMap.get("token");
+    public final static String APPID = ConstantMap.ConstantMap.get("appid");
+}
