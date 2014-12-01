14c474fe426fa24bdd7b5bd9936c5746fe2d4d05
Abner Lee <657143946@qq.com>
2014-12-01 20:11:12 +0800

    url constant

---
+++ src/main/java/me/abnerlee/wechatAPI/constant/UrlConstant.java
@@ -0,0 +1,34 @@
+package me.abnerlee.wechatAPI.constant;
+
+/**
+ * Created by AbnerLee on 14-12-1.
+ */
+
+/**
+ * 公众平台接口的链接地址
+ * 都带有默认的地址
+ */
+public class UrlConstant {
+    /**
+     * 申请平台access_token接口的地址
+     */
+    public final static String ACCESS_TOKEN_URL =
+            ConstantMap.CONSTANT_MAP.get("access_token_url") == null?
+                    "https://api.weixin.qq.com/cgi-bin/token":
+                    ConstantMap.CONSTANT_MAP.get("access_token_url");
+    /**
+     * 查询微信平台服务器的ip地址接口的地址
+     */
+    public final static String IP_LIST_URL =
+            ConstantMap.CONSTANT_MAP.get("ip_list_url") == null?
+                    "https://api.weixin.qq.com/cgi-bin/getcallbackip":
+                    ConstantMap.CONSTANT_MAP.get("ip_list_url");
+    /**
+     * 上传media到微信服务器接口的地址
+     */
+    public final static String MEDIA_UPLOAD_URL =
+            ConstantMap.CONSTANT_MAP.get("media_upload_url") == null?
+                    "http://file.api.weixin.qq.com/cgi-bin/media/upload":
+                    ConstantMap.CONSTANT_MAP.get("media_upload_url");
+
+}
