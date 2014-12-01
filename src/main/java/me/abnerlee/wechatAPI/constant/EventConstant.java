33a7ce129e852005ed3563c8ac7b367056641423
Abner Lee <657143946@qq.com>
2014-12-01 20:10:51 +0800

    add constant class

---
+++ src/main/java/me/abnerlee/wechatAPI/constant/EventConstant.java
@@ -0,0 +1,13 @@
+package me.abnerlee.wechatAPI.constant;
+
+/**
+ * Created by AbnerLee on 14-11-29.
+ */
+
+/**
+ * 推送事件消息的event字段的可能值
+ */
+public class EventConstant {
+    public final static String SUBSCRIBE = "subcribe";
+    public final static String UNSUBCRIBE = "subcribe";
+}
