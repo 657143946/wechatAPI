99a68efda80d21605fef541dc6658a0ca15eb020
Abner Lee <657143946@qq.com>
2014-12-01 20:12:27 +0800

    add annotation

--- src/main/java/me/abnerlee/wechatAPI/constant/AccountConstant.java
+++ src/main/java/me/abnerlee/wechatAPI/constant/AccountConstant.java
@@ -4,7 +4,11 @@ package me.abnerlee.wechatAPI.constant;
  * Created by AbnerLee on 14-12-1.
  */
 
+/**
+ * 微信平台的账号信息
+ */
 public class AccountConstant {
-    public final static String TOKEN = ConstantMap.ConstantMap.get("token");
-    public final static String APPID = ConstantMap.ConstantMap.get("appid");
+    public final static String TOKEN = ConstantMap.CONSTANT_MAP.get("token");
+    public final static String APPID = ConstantMap.CONSTANT_MAP.get("appid");
+    public final static String APPSECRET = ConstantMap.CONSTANT_MAP.get("appsecret");
 }
--- src/main/java/me/abnerlee/wechatAPI/constant/ConstantMap.java
+++ src/main/java/me/abnerlee/wechatAPI/constant/ConstantMap.java
@@ -7,6 +7,10 @@ import java.util.Map;
 /**
  * Created by AbnerLee on 14-12-1.
  */
+
+/**
+ * 所有常量信息，从wechat.properties中读取
+ */
 public class ConstantMap {
-    public final static Map<String, String> ConstantMap = PropertyReader.readProperties("wechat.properties");
+    public final static Map<String, String> CONSTANT_MAP = PropertyReader.readProperties("wechat.properties");
 }
