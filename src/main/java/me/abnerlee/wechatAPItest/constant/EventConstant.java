e40706d96406cc6968b8586c2a178e837a1a0cbc
Abner Lee <657143946@qq.com>
2014-12-01 21:21:46 +0800

    delete old api

--- src/main/java/me/abnerlee/wechatAPItest/constant/EventConstant.java
+++
@@ -1,9 +0,0 @@
-package me.abnerlee.wechatAPItest.constant;
-
-/**
- * Created by Administrator on 14-11-29.
- */
-public class EventConstant {
-    public final static String SUBSCRIBE = "subcribe";
-    public final static String UNSUBCRIBE = "subcribe";
-}
